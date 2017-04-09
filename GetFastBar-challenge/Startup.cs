ca66d082f3d3b79f5732aa1963384fcd141094f9
Blake Denniston <bd21@uw.edu>
2017-04-08 19:09:30 -0700

    Add project files.

---
+++ GetFastBar-challenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetFastBar_challenge.Startup))]
+namespace GetFastBar_challenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
